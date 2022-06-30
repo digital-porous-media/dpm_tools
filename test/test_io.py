c96a431d2f3acdd6a6f5dd41fcb4d77ea7165463
friedafarias <60853335+friedafarias@users.noreply.github.com>
2022-06-30 16:49:45 -0500

    Create test_io.py
    Began writing TestDPMTools skeleton code

---
+++ test/test_io.py
@@ -0,0 +1,30 @@
+import unittest
+import dpm_tools as dpm
+
+class TestDPMTools(unittest.TestCase):
+  def test_find_files(self):
+    #actual_tif = 
+    #expected_tif = 
+    #self.assertEqual(actual_tif, expected_tif)
+    
+    #actual_tiff = 
+    #expected_tiff = 
+    #self.assertEqual(actual_tiff, expected_tiff)
+    
+    #actual_raw = 
+    #expected_raw = 
+    #self.assertEqual(actual_raw, expected_raw)
+    
+    #actual_nc = 
+    #expected_nc = 
+    #self.assertEqual(actual_nc, expected_nc)
+    
+  def test_find_tiff_files(self):
+    #actual = 
+    #expected = 
+    #self.assertEqual(actual, expected)
+    
+  def test_evaluate_dimensions(self):
+    #actual_2D =
+    #expected_2D =
+    #self.assertEqual(actual_2D, expected_2D)
