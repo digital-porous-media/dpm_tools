d3de447f126d09a206461b061984f4a2049b6bd6
BC-Chang <bcchang@utexas.edu>
2022-07-06 17:31:40 -0500

    Added some requirements and file conversion script

--- dpm_tools/__init__.py
+++ dpm_tools/__init__.py
@@ -1,12 +1,12 @@
 import io
-import visualization
+# import visualization
 
 def _get_version():
-    from __version__ import __version__
+    from .__version__ import __version__
     suffix = "dev"
     if __version__.endswith(suffix):
         __version__ = __version__[:-len(suffix)]
     return __version__
 
 __version__ = _get_version()
-print(__version__)
+print(f'Version {__version__}')
