331d8fe782858e607e65d43069334795b291785e
Bernard Chang <bcchang@utexas.edu>
2022-07-07 16:54:29 -0500

    Added gif function, RAW needs work

--- dpm_tools/visualization/plot_2d.py
+++ dpm_tools/visualization/plot_2d.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 import numpy as np
+from tqdm import tqdm
 
-from ._vis_utils import _make_dir, _write_hist_csv
+from ._vis_utils import _make_dir, _write_hist_csv, _scale_image, AnimatedGif
 from ..__init__ import timer
+
+
+# TODO Add fig save decorator
 
 @timer
 def hist(data,
@@ -14,9 +19,9 @@ def hist(data,
     If save_fig is True, a save path should be supplied to kwargs under key "filepath"
     """
 
-    # Make line between bars black
-    if 'edgecolor' not in kwargs:
-        kwargs['edgecolor'] = 'k'
+    # # Make line between bars black
+    # if 'edgecolor' not in kwargs:
+    #     kwargs['edgecolor'] = 'k'
 
     # Set default figure size
     if 'fig_size' not in kwargs:
@@ -31,9 +36,9 @@ def hist(data,
     plt.tight_layout()
     plt.show()
 
-    # Create a figures directory if save path is not specified
-    if save_path is None:
-        save_path = f'{_make_dir("./figures")}'
+    # # Create a figures directory if save path is not specified
+    # if save_path is None:
+    #     save_path = f'{_make_dir("./figures")}'
 
     # TODO add write_csv with proper savepath
     if write_csv:
@@ -44,7 +49,7 @@ def hist(data,
     return fig
 
 @timer
-def plot_slice(data, slice_z: int = None, slice_axis: int = -1, **kwargs):
+def plot_slice(data, slice_num: int = None, slice_axis: int = 0, **kwargs):
 
     if 'origin' not in kwargs:
         kwargs['origin'] = 'lower'
@@ -52,10 +57,10 @@ def plot_slice(data, slice_z: int = None, slice_axis: int = -1, **kwargs):
     if 'interpolation' not in kwargs:
         kwargs['interpolation'] = 'none'
 
-    if slice_z is None:
-        slice_z = data.image.shape[slice_axis] // 2
+    if slice_num is None:
+        slice_num = data.image.shape[slice_axis] // 2
 
-    show_slice = data.image.take(indices=slice_z, axis=slice_axis)
+    show_slice = data.image.take(indices=slice_num, axis=slice_axis)
 
 
     fig = plt.figure(dpi=400)
@@ -67,6 +72,78 @@ def plot_slice(data, slice_z: int = None, slice_axis: int = -1, **kwargs):
     return fig
 
 @timer
-def thumbnail():
-    a = 1
+def make_thumbnail(data, thumb_slice: int = None, fig_size: tuple = (1, 1), slice_axis: int = 0, **kwargs):
+    if thumb_slice is None:
+        thumb_slice = int(np.floor(data.image.shape[slice_axis] / 2))
+
+    fig = plt.figure()
+    fig.set_size_inches(fig_size)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    plt.set_cmap('Greys')
+    show_slice = _scale_image(data.image).take(indices=thumb_slice, axis=slice_axis)
+    ax.imshow(show_slice, aspect='equal', vmin=0, vmax=1, **kwargs)
+    plt.show()
+
+    return fig
+
+
+@timer
+def make_gif(data,  dpi: int = 96, **kwargs):
+    """
+    Function to make and save a gif
+    """
+
+    sl1 = data.image[0, :, :]
+    images = []
+    animated_gif = AnimatedGif()
+    animated_gif.add(sl1, h=data.nx, w=data.ny)
+    slices = data.nz
+    print("Animation created for slice 1/" + str(slices))
+
+    if slices < 20:
+        slicesave = 1
+    elif slices < 100:
+        slicesave = 5
+    elif slices < 250:
+        slicesave = 12
+    else:
+        slicesave = 20
+
+    for i in range(1, slices, slicesave):
+        sl = data.image[i, :, :]
+        animated_gif.add(sl, h=data.nx, w=data.ny)
+        print("Animation created for slice " + str(i + 1) + "/" + str(slices))
+
+
+    animated_gif.save(f'{data.basepath}/{data.basename}.gif')
+
+    #
+    # # Save every ~n_slices/20 slice in gif
+    # if data.nz <= 20:
+    #     slice_save = 1
+    # else:
+    #     slice_save = int(np.round(data.nz / 20))
+    #
+    # images = [None]*20
+    #
+    # fig = plt.figure()
+    #
+    #
+    # for i, j in enumerate(range(0, data.nz, slice_save)):
+    #     fig.set_size_inches(data.nx / dpi, data.ny / dpi)
+    #     ax1 = plt.Axes(fig, [0., 0., 1., 1.])
+    #     ax1.set_axis_off()
+    #     fig.add_axes(ax1)
+    #     plt.set_cmap('Greys')
+    #     image_slice = _scale_image(data.image.take(indices=j, axis=0))
+    #     images[i] = ax1.imshow(image_slice, vmin=0, vmax=255, **kwargs)
+    #     plt.show()
+
+    # animation = anim.ArtistAnimation(fig, images)
+    # animation.save(f"{data.basepath}/{data.basename}.gif", writer='imagemagick', fps=60)
+
+    return images
+
 
