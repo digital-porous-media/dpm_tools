daaf24ddf9cec7685127f51bdf5c61feb6b0412b
Bernard Chang <bcchang@utexas.edu>
2022-07-07 12:00:19 -0500

    Added timing decorator

--- examples/stack_tiff.py
+++ examples/stack_tiff.py
@@ -1,6 +1,8 @@
 from dpm_tools.io import read_image
 from dpm_tools.visualization import hist
-
+import os
+import sys
+print(sys.path)
 
 
 image_info = {
@@ -13,4 +15,4 @@ image_info = {
 }
 # convert_filetype(filepath='../data/3_fractures.tif', convert_to='raw')#, metadata_dict=image_info)
 img = read_image(read_path='../data/35_1.tiff')
-my_hist = hist(img, write_csv=True)
+my_hist = hist(img, write_csv=False)
