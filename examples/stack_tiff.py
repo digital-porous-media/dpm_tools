52c8841b270c19156058422bc83489569ba379d3
Bernard Chang <bcchang@utexas.edu>
2022-07-18 15:33:10 -0500

    Update an example

--- examples/stack_tiff.py
+++ examples/stack_tiff.py
@@ -1,5 +1,8 @@
 from dpm_tools.io import Image, convert_filetype
 from dpm_tools.visualization import hist, plot_slice, make_thumbnail, make_gif
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
 
 image_info = {
@@ -10,13 +13,15 @@ image_info = {
     'ny': 110,
     'nz': 110,
 }
-convert_filetype(filepath='../data/berea_pore.raw', convert_to='tif', meta=image_info)
-img = Image(basepath='../data/', filename='berea_pore.tif')
+# convert_filetype(filepath='../data/berea_pore.raw', convert_to='tif', meta=image_info)
+img = Image(basepath='../data/', filename='berea_pore.raw', meta=image_info)
 
-# plot_slice(img, slice_num=25, cmap='gray')
+# for i in range(0, 110, 5):
+# plot_slice(img, slice_num=10, cmap='gray')
 # make_thumbnail(img)
 
-# TODO something wrong with using raw but ok using tiff
 make_gif(img)
+#
+# plt.show()
 # img = read_image(read_path='../data/35_1.tiff')
 # my_hist = hist(img.image, write_csv=False)
