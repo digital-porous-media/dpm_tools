7733a1e9181c91c080e36467a4dd908809daa018
Bernard Chang <bcchang@utexas.edu>
2024-02-20 11:19:18 -0600

    Modify docs to skip module file names

--- dpm_tools/metrics/__init__.py
+++ dpm_tools/metrics/__init__.py
@@ -1,4 +1,23 @@
-from .features import *
+r"""
+
+Pore scale characterization metrics
+###########################################################################
+
+.. currentmodule:: dpm_tools.metrics
+
+.. autosummary::
+    :template: base_tmpl.rst
+   :toctree:
+
+    slicewise_edt
+    edt
+    slicewise_mis
+    chords
+    time_of_flight
+    constriction_factor
+"""
+
+from ._features import *
 
 from ._feature_utils import *
 
