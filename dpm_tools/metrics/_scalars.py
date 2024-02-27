d3b7cbba0649994b19fec49d4400a56b24b7c2ae
Bernard Chang <bcchang@utexas.edu>
2024-02-27 14:05:27 -0600

    Create branch to include some more metrics

--- dpm_tools/metrics/_scalars.py
+++ dpm_tools/metrics/_scalars.py
@@ -1,1 +1,44 @@
-import quantimpy
+import numpy as np
+from quantimpy import minkowski as mk
+from typing import Tuple
+
+
+def minkowski_2d(data, **kwargs) -> Tuple[float, float, float]:
+    """
+    Compute the 2D Minkowski functionals (area, perimeter, Euler Characteristic)
+
+    Parameters:
+        data: An image dataclass containing the binary image where the phase of interest is 1.
+        **kwargs: Additional keyword arguments to be passed to the Quantimpy Minkowski functionals function
+    Returns:
+        Tuple[float, float, float]: Area, perimeter, radius of curvature
+    """
+
+    minkowski = mk.functionals(data.image.astype(bool), **kwargs)
+    minkowski[1] *= 2 * np.pi
+    minkowski[2] *= np.pi
+
+    return minkowski
+
+
+def minkowski_3d(data, **kwargs) -> Tuple[float, float, float, float]:
+    """
+    Compute the 3D scalar Minkowski functionals (volume, surface area, mean curvature, Euler Characteristic)
+
+    Parameters:
+        data: An image dataclass containing the binary image where the phase of interest is 1.
+        **kwargs: Additional keyword arguments to be passed to the Quantimpy Minkowski functionals function
+
+    Returns:
+        Tuple[float, float, float, float]: Volume, surface area, mean curvature, Euler characteristic
+    """
+
+    minkowski = mk.functionals(data.image.astype(bool), **kwargs)
+    minkowski[1] *= 8
+    minkowski[2] *= 2 * np.pi ** 2
+    minkowski[3] *= 4 * np.pi / 3
+
+    return minkowski
+
+
+# TODO: Minkowski Tensors
--- examples/morphological_features.py
+++ examples/morphological_features.py
@@ -1,4 +1,69 @@
 import numpy as np
-from dpm_tools.metrics import chords
+from dpm_tools.metrics import morphological_drainage_3d, edt
 from dpm_tools.io import ImageFromFile
+import matplotlib.pyplot as plt
 
+if __name__ == '__main__':
+    # Converting the image to "Image" class
+    img = ImageFromFile('../data', 'berea_pore.tif')
+    img.image = -1 * img.image + 1
+    # img.image = edt(img)
+    plt.figure()
+    plt.imshow(img.image[50], cmap="inferno")
+    plt.colorbar()
+    plt.show()
+
+
+    # Set initial radius based on the largest Euclidean distance
+    Rcrit_new = np.amax(edt(img))
+    Rcrit_old = np.amax(edt(img))
+    print(Rcrit_new)
+    Sw_drain = []
+    radii = []
+    total_global = np.count_nonzero(img.image)
+    target_saturation = 0.1
+    # void_fraction = total_global / img.image.size
+    saturation_new = 1.0
+    saturation_old = 1.0
+    saturation_diff_new = 1.0
+    saturation_diff_old = 1.0
+
+    # drain_config, saturation_new = morphological_drainage_3d(img, R_critical=Rcrit_new)
+
+
+    while saturation_new > target_saturation and Rcrit_new > 0.5:
+        saturation_diff_old = saturation_diff_new
+        saturation_old = saturation_new
+        Rcrit_old = Rcrit_new
+        Rcrit_new -= round(0.05*Rcrit_old)
+        # Generating the morphological drain configuration
+        drain_config, saturation_new = morphological_drainage_3d(img, R_critical=Rcrit_new)
+        saturation_diff_new = abs(saturation_new - target_saturation)
+        Sw_drain.append(saturation_new)
+        radii.append(Rcrit_new)
+
+        if saturation_diff_new < saturation_diff_old:
+            final_void_fraction = saturation_new
+        else:
+            final_void_fraction = saturation_old
+
+        print(f"{final_void_fraction}, {Rcrit_new}")
+
+
+    plt.figure(1)
+    # plt.subplot(1, 2, 1)
+    plt.title('Drainage: Rcrit=' + str(Rcrit_new) + ' Sw=' + str(saturation_new))
+    plt.pcolormesh(drain_config[:, :, img.nx // 2], cmap='hot')
+    # plt.pcolormesh(drain_config[20,:,:],cmap='hot')
+    plt.axis('equal')
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(2)
+    plt.plot(Sw_drain, radii,  'ro', markersize=6, label='Drainage')
+    plt.legend(loc='best')
+    plt.grid(True)
+    # plt.xlim([0, 1.0])
+    # plt.ylim([0, 2.0])
+
+    plt.show()
