96ef6cbf4e1b58f1a4f453e5a506db66db44cee0
BCChang <bcchang@utexas.edu>
2024-03-14 10:41:39 -0500

    Add metrics unit test and modify api to exclude file name

--- dpm_tools/visualization/__init__.py
+++ dpm_tools/visualization/__init__.py
@@ -25,10 +25,10 @@ This submodule contains functions for 2D and 3D visualization. 3D visualizations
     plot_medial_axis
 """
 
-from ._plot_2d import *
+from ._plot_2d import hist, plot_slice, make_thumbnail, make_gif, plot_heterogeneity_curve
 
-from ._plot_3d import *
+from ._plot_3d import orthogonal_slices, plot_isosurface, bounding_box, plot_glyph, plot_streamlines, plot_scalar_volume, plot_medial_axis
 
-from ._vis_utils import *
+# from ._vis_utils import *
 
-from ._3d_vis_utils import *
+# from ._3d_vis_utils import *
