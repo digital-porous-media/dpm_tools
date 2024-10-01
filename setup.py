55dad2c792b0c4b00b558ab0470913a113ab9d72
Bernard C. Chang <bcchang@utexas.edu>
2024-10-01 10:31:50 -0500

    Add cython to setup.py

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'edt',
         'scikit-fmm',
         'connected-components-3d',
+        'cython',
         'pyarrow'
     ],
     ext_modules=cythonize("dpm_tools/metrics/binary_configs.pyx"),
